5a626003f6ebbe4fa10b2f2cbed4056e82680845
liyche <liyche@umich.edu>
2019-03-06 14:45:04 -0500

    Add tests for additional keys features
    New tests include percentage using %, integer division using //,
    factorial using !, and bitwise operations AND &, OR |, and NOT ~.

--- test_rpn.py
+++ test_rpn.py
@@ -14,9 +14,29 @@ class TestBasics(unittest.TestCase):
     def test_divide(self):
         result = rpn.calculate('6 3 /')
         self.assertEqual(2, result)
-    def test_exponent(self):
-        result = rpn.calculate('2 3 ^')
-        self.assertEqual(8, result)
+    
+    # extension
     def test_decimal(self):
         result = rpn.calculate('2.3 6 +')
         self.assertEqual(8.3, result)
+    def test_percent(self):
+        result = rpn.calculate('20 25 % +')
+        self.assertEqual(25, result)
+    def test_exponent(self):
+        result = rpn.calculate('2 3 ^')
+        self.assertEqual(8, result)
+    def test_intdiv(self):
+        result = rpn.calculate('5 2 //')
+        self.asvsertEqual(2, result)
+    def test_factorial(self):
+        result = rpn.calculator('4 !')
+        self.assertEqual(24, result)
+    def test_bitAnd(self):
+        result = rpn.calculator('3 1 &')
+        self.assertEqual(1, result)
+    def test_bitOr(self):
+        result = rpn.calculator('3 1 |')
+        self.assertEqual(3, result)
+    def test_bitNot(self):
+        result = rpn.calculator('3 ~')
+        self.assertEqual(-4, result)
