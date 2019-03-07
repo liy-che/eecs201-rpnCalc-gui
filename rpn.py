2654a7092b682f19448eed6346f8f20f22abdd28
liyche <liyche@umich.edu>
2019-03-07 14:51:14 -0500

    Passing test case

--- rpn.py
+++ rpn.py
@@ -18,12 +18,22 @@ def calculate(arg):
             token = float(token)
             stack.append(token)
         except ValueError:
-            function = operators[token]
-            arg2 = stack.pop()
-            arg1 = stack.pop()
-            result = function(arg1, arg2)
-            stack.append(result)
-        print(stack)
+            try:
+                function = operators[token]
+                arg2 = stack.pop()
+                arg1 = stack.pop()
+                result = function(arg1, arg2)
+                op = ' ' + token
+            except KeyError:
+                if token == '!' and len(stack) != 1:
+                    numElt = len(stack)
+                    newArg = ' '.join(str(e) for e in stack)
+                    newArg = newArg + (op * (numElt - 1))
+                    result = calculate(newArg)
+                    del stack[:]
+            finally:   
+                stack.append(result)
+    print(stack)
     if len(stack) != 1:
         raise TypeError("Too many parameters")
     return stack.pop()
